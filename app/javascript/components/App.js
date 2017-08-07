456c9770e1335ac1406dd3ffa408fe883b4a67e2
Danil Galeev <danil.galeev@sap.com>
2017-08-07 22:29:32 +0200

    Init basic page structure

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 
-import Header from './Header'
-import Main from './Main'
-import Footer from './Footer'
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 export default class App extends React.Component {
     render() {
--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+import Navigation from './Navigation';
+import logo from '../../assets/images/logo.jpg';
 
 export default class Header extends React.Component {
     render() {
         return (
-            <div>Header</div>
+            <header>
+                <div className="container">
+                    <div className="logo">
+                        <a href='/'>
+                            <img src={logo} alt="Pick Or Boo!" />
+                        </a>
+                    </div>
+                    <Navigation />
+                </div>
+                <div className="delimiter"></div>
+            </header>
         );
     }
 }
