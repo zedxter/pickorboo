a1beb2ee324b65d169da270f296fd3bdd4f6ddce
Danil Galeev <danil.galeev@sap.com>
2017-08-08 09:42:04 +0200

    Add materials-ui && small refactorings

--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default class Footer extends React.Component {
     render() {
         return (
-            <footer>Footer</footer>
+            <footer>
+                <div className="container">
+                    <span className="copyright">© 2017 PickOrBoo</span>
+                </div>
+            </footer>
         );
     }
 }
--- app/javascript/components/Main.js
+++ app/javascript/components/Main.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default class Main extends React.Component {
     render() {
         return (
-            <div className="main">Main Content</div>
+            <div className="main">
+                <div className="container">
+                    Main Content
+                </div>
+            </div>
         );
     }
 }
