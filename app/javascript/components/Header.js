c3f8ee221e67aae5af4c21606a273f299f01603b
Danil Galeev <danil.galeev@sap.com>
2017-08-08 21:17:33 +0200

    Add eslint config && dependencies

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Navigation from './Navigation';
 import logo from '../../assets/images/logo.jpg';
 
-export default class Header extends React.Component {
+export default class Header extends React.PureComponent {
     render() {
         return (
             <header>
--- app/javascript/components/Main.js
+++ app/javascript/components/Main.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+import PollItem from './PollItem';
 
 export default class Main extends React.Component {
     render() {
+        const items = [];
+        const content = items.map((item) =>
+            <PollItem item={item} />
+        );
         return (
             <div className="main">
                 <div className="container">
-                    Main Content
+                    {content}
                 </div>
             </div>
         );
